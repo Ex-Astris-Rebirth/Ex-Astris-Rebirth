a471017349b1bd924a4f563717498aadf0511b9c
jenkins <im@loveholy.kr>
2015-05-14 00:21:36 +0900

    compatibility bug fix

--- src/main/java/kr/loveholy/exastrisrebirth/compatibility/ModRedstoneArsenal.java
+++ src/main/java/kr/loveholy/exastrisrebirth/compatibility/ModRedstoneArsenal.java
@@ -1,8 +1,22 @@
 package kr.loveholy.exastrisrebirth.compatibility;
 
+import net.minecraft.item.Item;
+import kr.loveholy.exastrisrebirth.item.ItemCrookRF;
+import kr.loveholy.exastrisrebirth.item.ItemHammerRF;
+
 public class ModRedstoneArsenal {
 	public static void init()
 	{
 		
+	}
+	
+	public static Item itemHammerRF()
+	{
+		return new ItemHammerRF();
+	}
+	
+	public static Item itemCrookRF()
+	{
+		return new ItemCrookRF();
 	}
 }
